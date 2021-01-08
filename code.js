518ddf77a592dd81711633442648458f1af63957
Jim Stewart <james.h.stewart@gmail.com>
2021-01-07 22:22:18 -0800

    Days til inagration
    Days til inagration

---
+++ code.js
@@ -0,0 +1,8 @@
+daysLeft = new Date("January 20, 2021 12:00")  - Date.now();
+saramucci = new Date("August 1, 2017") -  new Date("July 20, 2017");
+muccis = (daysLeft/saramucci);
+//document.getElementById("days").innerHTML = muccis + " saramuccis"
+
+console.log(daysLeft);
+console.log(saramucci);
+console.log(muccis);
